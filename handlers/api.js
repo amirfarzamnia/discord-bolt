ac370ddd5db73e2bd503230ed762422f9c7e6ede
amirfarzamnia <amir2452fa@gmail.com>
2024-07-20 13:19:51 +0330

    Improved comments

--- handlers/api.js
+++ handlers/api.js
@@ -26,15 +26,15 @@ export default class API {
         /** Discord API base URL. */
         this.api = 'https://discord.com/api';
 
-        /** Assigning optional parameters. */
+        // Assigning optional parameters.
         Object.assign(this, options);
 
-        /** Dynamically create methods based on imported API methods */
+        // Dynamically create methods based on imported API methods
         Object.entries(methods).forEach(([name, { method, endpoint }]) => {
-            /** Convert method name to camelCase */
+            // Convert method name to camelCase
             name = name.charAt(0).toLocaleLowerCase() + name.slice(1);
 
-            /** Replace dashes and spaces */
+            // Replace dashes and spaces
             name = name.replace(/-/g, ' ').replace(/ /g, '');
 
             /**
@@ -47,66 +47,66 @@ export default class API {
              */
 
             this[name] = (parameters, headers) => {
-                /** The data received from the promise. */
+                // The data received from the promise.
                 const promise = new Promise((resolve, reject) => {
-                    /** The address of the target URL. */
+                    // The address of the target URL.
                     const address = `${this.api}/v${this.version}/${endpoint.replace(/{([^}]+)}/g, (_, i) => parameters[i])}`;
 
-                    /** Constructing the full URL for the API endpoint. */
+                    // Constructing the full URL for the API endpoint.
                     const url = new URL(address);
 
-                    /** If the request method is GET, encode parameters into the query string. */
+                    // If the request method is GET, encode parameters into the query string.
                     if (method === 'GET') url.search = new URLSearchParams(parameters).toString();
 
-                    /** Construct request options */
+                    // Construct request options
                     const options = {
-                        /** The specified request method. */
+                        // The specified request method.
                         method,
-                        /** The request's headers. */
+                        // The request's headers.
                         headers: {
-                            /** This header is used for the bot's authentication process. */
+                            // This header is used for the bot's authentication process.
                             'Authorization': `Bot ${this.token}`,
-                            /** The request's content-type. */
+                            // The request's content-type.
                             'Content-Type': 'application/json',
-                            /** Optional request headers. */
+                            // Optional request headers.
                             ...headers
                         }
                     };
 
-                    /** Making the HTTP request. */
+                    // Making the HTTP request.
                     const req = https.request(url, options, (res) => {
-                        /** The template where the new incoming data will be written. */
+                        // The template where the new incoming data will be written.
                         let data = '';
 
-                        /** Collect the response data. */
+                        // Collect the response data.
                         res.on('data', (chunk) => (data += chunk));
 
-                        /** Handle the end of the response. */
+                        // Handle the end of the response.
                         res.on('end', () => {
                             try {
-                                /** Try to parse the JSON data and resolve it. */
+                                // Try to parse the JSON data and resolve it.
                                 resolve(JSON.parse(data));
                             } catch {
-                                /** If the parsing process failed, return the data if it exists; if not, return an empty object. */
+                                // If the parsing process failed, return the data if it exists; if not, return an empty object.
                                 resolve(data || {});
                             }
                         });
                     });
 
-                    /** Handle request errors. */
+                    // Handle request errors.
                     req.on('error', (error) => reject(error));
 
-                    /** Include request body if method is not GET. */
+                    // Include request body if method is not GET.
                     if (method !== 'GET') req.write(JSON.stringify(parameters));
 
-                    /** End the request. */
+                    // End the request.
                     req.end();
                 });
 
-                /** If the promise faced rate limitations, check if it should retry, and if it should, retry sending the request. */
+                // If the promise faced rate limitations, check if it should retry, and if it should, retry sending the request.
                 promise.then(({ retry_after }) => retry_after && this.rateLimitRetry && setTimeout(() => this[name](parameters, headers), retry_after * 1000));
 
-                /** Return the data received from the promise. */
+                // Return the data received from the promise.
                 return promise;
             };
         });
